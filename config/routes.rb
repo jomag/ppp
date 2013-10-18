27f7fbf8c66c8a0e823825f82b0ab070c935ccc7
Jonatan Magnusson <jonatan.magnusson@gmail.com>
2013-10-18 22:13:01 +0200

    Added part model and start controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Ppp::Application.routes.draw do
+  get "start/index"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'start#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
